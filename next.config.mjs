1cca2fbf4811861984505053fbd81a0eb4934728
EricWu <skynocover@gmail.com>
2024-10-26 15:15:29 +0800

    workers的設定
    chore: 新增gitignore 並移除ISR相關程式碼
    
    chore: 測試部署
    
    chore: 修正部署的錯誤
    
    chore: 更新github action
    
    chore: 修正github action
    
    chore: 修正github action
    
    chore: 修正CICD
    
    chore: 設定CICD

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     domains: ['eu-central-1.storage.xata.sh'],
   },
+  webpack: (config, { isServer }) => {
+    config.externals = [...config.externals, 'esbuild'];
+
+    if (!isServer) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        sideEffects: true,
+        usedExports: true,
+      };
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
